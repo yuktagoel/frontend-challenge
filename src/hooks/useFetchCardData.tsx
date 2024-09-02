7d338096c8c859c6ed8d2d9bdaf5e19fe6ddfb3d
Yukta Goel <yukta.goyal25@gmail.com>
2024-09-03 00:36:09 +0530

    add: hook

---
+++ src/hooks/useFetchCardData.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { DataItem } from "../types/types";
+
+
+export const useFetchData = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data.json');
+        if (!response.ok) throw new Error('Network response was not ok');
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'An unknown error occurred');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  },[]); 
+
+  return { data, loading, error };
+};
