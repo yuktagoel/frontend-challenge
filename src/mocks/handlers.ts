362f2229fb234a85142fec70c890e1141827071c
Yukta Goel <yukta.goyal25@gmail.com>
2024-09-04 14:31:32 +0530

    add: msw POST call

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,26 @@
 import { http, HttpResponse } from "msw";
 
+const DOCUMENTS_STORAGE_ID = "documents";
+
 export const handlers = [
   http.get("https://api.com/documents", async () => {
-    
-    const response = await fetch('/data.json');
+    const storedData = localStorage.getItem(DOCUMENTS_STORAGE_ID);
+    if (storedData) return HttpResponse.json(JSON.parse(storedData));
+
+    const response = await fetch("/data.json");
     const data = await response.json();
-    
+
+    localStorage.setItem(DOCUMENTS_STORAGE_ID, JSON.stringify(data));
     return HttpResponse.json(data);
+  }),
+
+  http.post("https://api.com/documents", async ({request}) => {
+    const newData = await request.json();
+    localStorage.setItem(DOCUMENTS_STORAGE_ID, JSON.stringify(newData));
+    
+    return HttpResponse.json({
+      message: "Data saved successfully",
+      data: newData
+    });
   }),
 ];
